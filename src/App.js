e3c707ad84ac857fb9ffa2dbcdacdd27f511e4d7
Ava Choi <alexbchoi@gmail.com>
2021-09-16 18:45:58 -0400

    onClick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SingleDrink from "./SingleDrink";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			drinkList: [],
+			userInput: "",
+			singleDrink: {},
+		};
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onButtonClick = this.onButtonClick.bind(this);
+		this.singleDrinkDetail = this.singleDrinkDetail(this);
+	}
+
+	componentDidMount() {}
+
+	onInputChange(e) {
+		this.setState({
+			userInput: e.target.value,
+		});
+	}
+
+	onButtonClick(e) {
+		let drinkType = this.state.userInput;
+		fetch(
+			"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkType
+		)
+			.then((res) => res.json())
+			.then((result) => {
+				this.setState({
+					drinkList: result.drinks,
+				});
+			});
+	}
+
+	singleDrinkDetail(e) {
+		console.log("event", e);
+		// this.setState({singleDrink:})
+		// fetch("www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id)
+		// 	.then((res) => res.json())
+		// 	.then((result) => {
+		// 		this.setState({
+		// 			singleDrink: result.drinks,
+		// 		});
+		// 	});
+	}
+
+	render() {
+		console.log("this.state", this.state);
+
+		let data = this.state.drinkList;
+		let singleDrink = this.state.singleDrink;
+
+		if (singleDrink.id) {
+			return (
+				<div>
+					<SingleDrink drink={singleDrink} />;
+				</div>
+			);
+		} else {
+			if (data.length) {
+				return (
+					<div className="App">
+						<input type="text" onChange={this.onInputChange} />
+						<input
+							type="button"
+							value="Enter a drink"
+							onClick={this.onButtonClick}
+						/>
+						<ul>
+							{data.map((drink) => (
+								<li
+									key={drink.idDrink}
+									onClick={() => this.singleDrinkDetail(drink.idDrink)}
+								>
+									{drink.strDrink}
+									<img
+										src={drink.strDrinkThumb}
+										alt=""
+										onClick={this.singleDrinkDetail}
+									/>
+								</li>
+							))}
+						</ul>
+					</div>
+				);
+			} else {
+				return (
+					<div className="App">
+						<input type="text" onChange={this.onInputChange} />
+						<input
+							type="button"
+							value="Enter a drink"
+							onClick={this.onButtonClick}
+						/>
+					</div>
+				);
+			}
+		}
+	}
 }
 
 export default App;
---
+++ src/SingleDrink.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+const SingleDrink = (props) => {
+	// const drink = props;
+	console.log("props", props);
+};
+
+export default SingleDrink;
+
+// let singleDrinkId= this.state.singleDrink;
+//     fetch(
+//       "www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + singleDrinkId
+//     )
+//     .then((res) => res.json())
+//     .then((result)=>{
+//       this.setState({
+//         singleDrink: result.drinks
+//       })
+//     })
+
+// class singleDrink extends React.Component{
+//     render(){
+//         const drink =
+//         return(
+//             <div>
+//                 <div>{drink.name}</div>
+//             </div>
+//         )
+//     }
+// }
